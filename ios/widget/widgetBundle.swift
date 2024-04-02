d0317af32de83d80b413f646f1f65d1d97aef9cc
Rishi Chhabra <Dakshrishi123@gmail.com>
2024-04-02 12:53:58 +0530

    Notifications and widget not working

---
+++ ios/widget/widgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  widgetBundle.swift
+//  widget
+//
+//  Created by Rishi Chhabra on 01/04/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetBundle: WidgetBundle {
+    var body: some Widget {
+        widget()
+        widgetLiveActivity()
+    }
+}
